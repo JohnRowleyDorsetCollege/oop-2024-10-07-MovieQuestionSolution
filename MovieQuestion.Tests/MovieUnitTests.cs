ea56073d3532bcdaa8dc94b6d2449bbc5cf59358
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-10-07 14:47:40 +0100

    Added Tests

--- MovieQuestion.Tests/MovieUnitTests.cs
+++ MovieQuestion.Tests/MovieUnitTests.cs
@@ -1,10 +1,26 @@
+using MovieQuestion.Domain;
+
 namespace MovieQuestion.Tests
 {
     public class MovieUnitTests
     {
         [Fact]
-        public void Test1()
+        public void MovieSetRating_Sets_The_Correct_Rating()
         {
+            Movie testMovie = new Movie();
+
+            testMovie.SetRating("PG");
+
+            Assert.Equal("PG", testMovie.Rating);
+        }
+
+        [Fact]
+        public void MovieSetRating_Throws_Exception_When_Rating_Is_Incorrect()
+        {
+            Movie testMovie = new Movie();
+
+            Assert.Throws<InvalidDataException>(() => testMovie.SetRating("INVALIDRATING"));
+
 
         }
     }
